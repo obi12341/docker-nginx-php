e70dae89284c5a83125187ccb8f22227394d49bf
Patrick Oberdorf <p.oberdorf@raphael-gmbh.de>
2016-04-20 13:38:42 +0200

    added test

---
+++ Dockerfile_spec.rb
@@ -0,0 +1,74 @@
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    if ENV['IMAGEID']
+      image = Docker::Image.get(ENV['IMAGEID'])
+    else
+      image = Docker::Image.build_from_dir('.')
+    end
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  describe package('nginx') do
+    it { should be_installed }
+  end
+
+  describe package('php5-fpm') do
+    it { should be_installed }
+  end
+
+  describe package('supervisor') do
+    it { should be_installed }
+  end
+
+  describe package('ssmtp') do
+    it { should be_installed }
+  end
+
+  describe service('nginx') do
+    it "nginx should be running under supervisor" do
+      wait_retry 30 do
+        should be_running.under('supervisor')
+      end
+    end
+  end
+
+  describe port(80) do
+    it "nginx should be listening" do
+      wait_retry 30 do
+        should be_listening
+      end
+    end
+  end
+
+  describe service('php-fpm') do
+    it "php-fpm should be running under supervisor" do
+      wait_retry 30 do
+        should be_running.under('supervisor')
+      end
+    end
+  end
+
+  describe file('/var/run/php5-fpm.sock') do
+    it { should be_socket }
+  end
+
+end
+
+def wait_retry(time, increment = 1, elapsed_time = 0, &block)
+  begin
+    yield
+  rescue Exception => e
+    if elapsed_time >= time
+      raise e
+    else
+      sleep increment
+      wait_retry(time, increment, elapsed_time + increment, &block)
+    end
+  end
+end
