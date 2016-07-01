1aecd390c9031e1f5035188dae75acdd917ff604
Patrick Oberdorf <p.oberdorf@raphael-gmbh.de>
2016-07-01 13:36:48 +0200

    added abilty to specify inet_protocol

--- parent.sh
+++ parent.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 POSTFIX_HOSTNAME=${POSTFIX_HOSTNAME:-example.com}
+POSTFIX_PROTOCOL=${POSTFIX_PROTOCOL:-ipv4}
 sed -i "s/{{POSTFIX_HOSTNAME}}/${POSTFIX_HOSTNAME}/" /etc/postfix/main.cf
+sed -i "s/{{POSTFIX_PROTOCOL}}/${POSTFIX_PROTOCOL}/" /etc/postfix/main.cf
