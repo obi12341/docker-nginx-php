b7427629bac71be5e5cbb67b3138743c4b1adb7b
Patrick Oberdorf <p.oberdorf@raphael-gmbh.de>
2016-07-01 14:37:49 +0200

    postfix: added abbility to specify relayhost

--- parent.sh
+++ parent.sh
@@ -2,5 +2,8 @@
 
 POSTFIX_HOSTNAME=${POSTFIX_HOSTNAME:-example.com}
 POSTFIX_PROTOCOL=${POSTFIX_PROTOCOL:-ipv4}
+POSTFIX_RELAYHOST=${POSTFIX_RELAYHOST:-}
+
 sed -i "s/{{POSTFIX_HOSTNAME}}/${POSTFIX_HOSTNAME}/" /etc/postfix/main.cf
 sed -i "s/{{POSTFIX_PROTOCOL}}/${POSTFIX_PROTOCOL}/" /etc/postfix/main.cf
+sed -i "s/{{POSTFIX_RELAYHOST}}/${POSTFIX_RELAYHOST}/" /etc/postfix/main.cf
